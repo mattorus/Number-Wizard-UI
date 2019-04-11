4f0b73177c295106351450d4e1d75483d747a74e
mattorus <mattorus@gmail.com>
2019-04-11 11:20:45 -0700

    First commit

---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Assets;
+
+public class NumberWizard : MonoBehaviour
+{
+    [SerializeField] int max;
+    [SerializeField] int min;
+    [SerializeField] TextMeshProUGUI guessText;
+
+    private TreeNode root;
+    private TreeNode tree;
+    private TreeNode guessNode;    
+    
+    // Game 
+    int guess;
+    private bool IsGameOver;
+    private readonly int higher = +1;
+    private readonly int lower = -1;
+    
+    // Start is called before the first frame update
+    public void Start()
+    {
+        ResetGame();
+    }
+
+    public void OnPressHigher()
+    {
+        min = guess + 1;
+        UpdateGuess(higher);
+    }
+
+    public void OnPressLower()
+    {
+        max = guess;
+        UpdateGuess(lower);
+    }
+
+    private void NewGuess(int newMin, int newMax)
+    {
+        guess = Random.Range(min, max);
+        guessText.text = guess.ToString();
+    }
+
+    private void UpdateGuess(int direction)
+    {
+        NewGuess(min, max);
+
+        tree.Add(guess);
+        if (direction > 0)
+        {
+            tree = tree.Right;
+        }
+        else
+        {
+            tree = tree.Left;
+        }        
+    }
+
+    private void ResetGame()
+    {           
+        IsGameOver = false;
+        NewGuess(min, max + 1);
+        root = new TreeNode(guess);
+        tree = root;
+    }
+
+    public void Victory()
+    {
+        int guessCount = 0;
+        Queue<TreeNode> guessQueue = new Queue<TreeNode>();
+        guessQueue.Enqueue(root);
+        while (guessQueue.Count > 0)
+        {
+            guessCount++;
+            root = guessQueue.Dequeue();
+            
+            if (root.Left != null)
+            {
+                guessQueue.Enqueue(root.Left);
+            }
+            if (root.Right != null)
+            {
+                guessQueue.Enqueue(root.Right);
+            }
+        }
+        
+        IsGameOver = true;
+    }
+
+    public void QuitGame()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else        
+            Application.Quit();
+        #endif
+    }
+}
