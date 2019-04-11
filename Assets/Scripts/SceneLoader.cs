f34170f0293abb64fee24ed8cdae6b59cb9aa872
mattorus <mattorus@gmail.com>
2019-04-11 12:30:34 -0700

    Complete UI & scene navigation

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -18,4 +18,13 @@ public class SceneLoader : MonoBehaviour
         
         SceneManager.LoadScene(currentSceneIndex + 1);
     }
+
+    public void QuitGame()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
 }
